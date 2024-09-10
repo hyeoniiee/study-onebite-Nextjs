dd80e3bf53c34641d243a4cf657043a63bf1bf5d
hyeoniiee <starjea0126@gmail.com>
2024-09-10 21:10:13 +0900

    day03

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,6 +1,37 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/test");
+  };
+
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+  // 프로그래메틱하게 이동시키는 페이지에 대해서도 프리패칭을 시키고 싶다면 router 객체의 prefetch 메서드를 통해 특정 페이지를 명시적으로 프리패칭하도록 설정할 수 있다.
+
+  return (
+    <>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
+        {/* prefetch의 값을 flase 로 설정하면 프리패칭을 명시적으로 해제할 수 있다. */}
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -6,3 +6,7 @@ export default function Page() {
 
   return <h1>Search {q}</h1>;
 }
+
+// 폴더로 관리하고 싶으면 폴더하위에 index.tsx로 작성해야 한다.
+// pages/search.tsx (O)
+// pages/search/search.tsx (X) -> pages/search/index.tsx (O)

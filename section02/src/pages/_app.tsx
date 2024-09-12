280522e87de2eacc6efcba69c2b4c3f4d87ee054
hyeoniiee <starjea0126@gmail.com>
2024-09-12 22:59:26 +0900

    day04

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,37 +1,20 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
 
-  const onClickButton = () => {
-    router.push("/test");
-  };
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: NextPageWithLayout;
+}) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
-  useEffect(() => {
-    router.prefetch("/test");
-  }, []);
-  // 프로그래메틱하게 이동시키는 페이지에 대해서도 프리패칭을 시키고 싶다면 router 객체의 prefetch 메서드를 통해 특정 페이지를 명시적으로 프리패칭하도록 설정할 수 있다.
-
-  return (
-    <>
-      <header>
-        <Link href={"/"}>index</Link>
-        &nbsp;
-        <Link href={"/search"} prefetch={false}>
-          search
-        </Link>
-        {/* prefetch의 값을 flase 로 설정하면 프리패칭을 명시적으로 해제할 수 있다. */}
-        &nbsp;
-        <Link href={"/book/1"}>book/1</Link>
-        <div>
-          <button onClick={onClickButton}>/test 페이지로 이동</button>
-        </div>
-      </header>
-      <Component {...pageProps} />
-    </>
-  );
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,4 +1,6 @@
+import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
 
 export default function Page() {
   const router = useRouter();
@@ -6,6 +8,10 @@ export default function Page() {
 
   return <h1>Search {q}</h1>;
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
 
 // 폴더로 관리하고 싶으면 폴더하위에 index.tsx로 작성해야 한다.
 // pages/search.tsx (O)

853bb75a6c4a9c1bdd65b8101938722cc7bca119
hyeoniiee <starjea0126@gmail.com>
2024-09-12 23:24:30 +0900

    day04-challenge

--- onebite-cinema/src/pages/index.tsx
+++ onebite-cinema/src/pages/index.tsx
@@ -1,3 +1,11 @@
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode } from "react";
+
 export default function Home() {
-  return <h2>ONEBITE CINEMA</h2>;
+  // return <h2>ONEBITE CINEMA</h2>;
+  return <div></div>;
 }
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};

1dd43c15efe5a7bc026101f0f4f3db46c9400b36
hyeoniiee <starjea0126@gmail.com>
2024-09-21 13:44:49 +0900

    day07-challenge

--- onebite-cinema/src/pages/movie/[id].tsx
+++ onebite-cinema/src/pages/movie/[id].tsx
@@ -1,13 +1,31 @@
 import style from "./[id].module.css";
-//import { useRouter } from "next/router";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import fetchMovies from "@/lib/fetch-movies";
 import fetchOneMovie from "@/lib/fetch-one-movie";
+import { useRouter } from "next/router";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticPaths = async () => {
+  const movies = await fetchMovies();
+  return {
+    paths: movies.map(({ id }) => ({
+      params: { id: id.toString() },
+    })),
+    fallback: true,
+  };
+};
+
+// export const getServerSideProps = async (
+//   context: GetServerSidePropsContext
+// ) => {
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const movie = await fetchOneMovie(Number(id));
+
+  if (!movie) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
@@ -18,7 +36,10 @@ export const getServerSideProps = async (
 
 export default function Page({
   movie,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) return "로딩중입니다.";
   if (!movie) return "문제가 발생했습니다. 다시 시도해 주세요";
   const {
     id,

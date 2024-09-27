eea304ba3e60f53acf9eb8a38b954f1c79100054
hyeoniiee <starjea0126@gmail.com>
2024-09-28 00:24:01 +0900

    day12-challenge

--- onebite-cinema-app/src/app/(with-searchbar)/page.tsx
+++ onebite-cinema-app/src/app/(with-searchbar)/page.tsx
@@ -1,25 +1,58 @@
 import MovieItem from "@/components/movie-item";
 import style from "./page.module.css";
-import movies from "@/mock/dummy.json";
+import { MovieData } from "@/types";
+
+async function AllMovies() {
+  // 전체 영화 데이터는 새로운 영화가 추가되거나 삭제되지 않으므로 "force-cache" 적용
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    { cache: "force-cache" }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+
+  const allMovies: MovieData[] = await response.json();
+  return (
+    <div className={style.all_container}>
+      {allMovies.map((movie) => (
+        <MovieItem key={`all-${movie.id}`} {...movie} />
+      ))}
+    </div>
+  );
+}
+
+async function RecoMovies() {
+  // 랜덤으로 제공되는 데이터이므로 추천 영화를 변경하기 위해 "revalidate" 적용
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
+    { next: { revalidate: 3 } }
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const recoMovies: MovieData[] = await response.json();
+  return (
+    <div className={style.reco_container}>
+      {recoMovies.map((movie) => (
+        <MovieItem key={`reco-${movie.id}`} {...movie} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <div className={style.conatiner}>
+    <div className={style.container}>
       <section>
-        <h3>지금 가장 추천하는 영화</h3>
-        <div className={style.reco_conatiner}>
-          {movies.slice(0, 3).map((movie) => (
-            <MovieItem key={`recomovie-${movie.id}`} {...movie} />
-          ))}
-        </div>
+        <h3>지금 추천하는 도서</h3>
+        <RecoMovies />
       </section>
       <section>
-        <h3>등록된 모든 영화</h3>
-        <div className={style.all_container}>
-          {movies.map((movie) => (
-            <MovieItem key={`allmovie-${movie.id}`} {...movie} />
-          ))}
-        </div>
+        <h3>등록된 모든 도서</h3>
+        <AllMovies />
       </section>
     </div>
   );

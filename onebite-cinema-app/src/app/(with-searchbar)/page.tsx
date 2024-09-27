acb630c0e2708b2a96e7be3ed95143fe0b8c16dc
hyeoniiee <starjea0126@gmail.com>
2024-09-28 00:34:33 +0900

    오타수정

--- onebite-cinema-app/src/app/(with-searchbar)/page.tsx
+++ onebite-cinema-app/src/app/(with-searchbar)/page.tsx
@@ -47,11 +47,11 @@ export default function Home() {
   return (
     <div className={style.container}>
       <section>
-        <h3>지금 추천하는 도서</h3>
+        <h3>지금 가장 추천하는 영화</h3>
         <RecoMovies />
       </section>
       <section>
-        <h3>등록된 모든 도서</h3>
+        <h3>등록된 모든 영화</h3>
         <AllMovies />
       </section>
     </div>

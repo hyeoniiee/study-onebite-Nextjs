845d79745e6cfede73f0efc846b9fc0b4929e5f6
hyeoniiee <starjea0126@gmail.com>
2024-09-30 22:57:59 +0900

    day15 - 스트리밍2 ~ 에러핸들링

--- section06/src/app/(with-searchbar)/search/page.tsx
+++ section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,8 @@
 import BookItem from "@/components/book-item";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
 // export const dynamic = "force-static";
 // 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
@@ -9,16 +11,10 @@ import { delay } from "@/util/delay";
 // 3. force-static : 페이지를 강제로 Static 페이지로 설정
 // 4. error : 페이지를 강제로 Static 페이지 설정 (설정하면 안되는 이유 -> 빌드 오류)
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: {
-    q?: string;
-  };
-}) {
+async function SearchResult({ q }: { q: string }) {
   await delay(1500);
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     { cache: "force-cache" }
   );
   if (!response.ok) {
@@ -33,5 +29,23 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  return (
+    // <Suspense key={searchParams.q || ""} fallback={<div>Loading ...</div>}>
+    <Suspense
+      key={searchParams.q || ""}
+      fallback={<BookListSkeleton count={3} />}
+    >
+      <SearchResult q={searchParams.q || ""} />
+    </Suspense>
   );
 }
